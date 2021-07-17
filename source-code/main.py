4fdead781549328f14f84e63a5f2a46f7d021418
Lucas Viana <oviana@outlook.com>
2021-07-17 20:52:32 -0300

    updates main.py to loop through all queries and save the results

--- source-code/main.py
+++ source-code/main.py
@@ -1,6 +1,6 @@
 from index_class import Index
-from util import *
 from methods import probabilisticModel, vectorialModel
+from util import *
 
 FILTER_STOPWORDS = True
 STEM_WORDS = True
@@ -8,7 +8,14 @@ STEM_WORDS = True
 database_contents = get_db_content()
 index = Index(database_contents, FILTER_STOPWORDS, STEM_WORDS)
 
-query = parse_text('Clashes between the Gurjars and Meenas', FILTER_STOPWORDS, STEM_WORDS)
+queries = get_queries()
 
-print(probabilisticModel(query, index))
-print(vectorialModel(query, index))
+results = [
+    {
+        'query': query['id'],
+        'results': {
+            'probabilistic': probabilisticModel(query['query'], index),
+            'vectorial': vectorialModel(query['query'], index)
+        }
+    } for query in queries
+]
