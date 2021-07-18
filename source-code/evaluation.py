8c907daccfdd85aa9fa2806d4622774c115d3e0c
Lucas Viana <oviana@outlook.com>
2021-07-18 10:40:16 -0300

    finishes Evaluation.dcg() and adds documentation

--- source-code/evaluation.py
+++ source-code/evaluation.py
@@ -2,13 +2,53 @@ import math
 
 
 class Evaluation:
+    """
+    A class to performs various evaluation methods for a single query given to
+    a IR method.
+
+    Parameters:
+        returned_set (list): set of ranked documents returned by the IR method.
+        truth_set (list): set of actually relevant documents for that same
+                          query.
+    """
+
     def __init__(self, returned_set: list, truth_set: list):
         self.returned_set = returned_set
         self.truth_set = truth_set
 
-    def dcg(self):
-        gain = [ 1. if doc in self.truth_set else 0. for doc in self.returned_set ]
+        self.__dcg = None
+        self.__idcg = None
 
-        dcg = [ gain[0] ]
-        for i in range(1, len(gain)):
-            dcg.append(gain[i]/math.log2(i+1) + dcg[i-1])
+    def dcg(self) -> tuple:
+        """
+        Performs the DCG (discounted accumulated gain) evaluation for this
+        query and returns the DCG vector as well as the IDCG (the DCG's ideal
+        version).
+
+        In order to use the DCG to compare multiple IR models, you must
+        perform the DCG for many queries and then use the quocient value
+        between the mean DCG and the mean IDCG.
+
+        In order to better compare the two models, you may plot this results
+        with the document list as the x-axis.
+
+        Return value:
+            tuple: the first element is the DCG and the second is the IDCG.
+        """
+
+        if not self.__dcg and not self.__idcg:
+            gain = [ 1. if doc in self.truth_set else 0. for doc in self.returned_set ]
+            ideal_gain = sorted(gain, reverse=True)
+
+            dcg = [ gain[0] ]
+            for i in range(1, len(gain)):
+                dcg.append(gain[i]/math.log2(i+1) + dcg[i-1])
+
+            idcg = [ ideal_gain[0] ]
+            for i in range(1, len(ideal_gain)):
+                idcg.append(ideal_gain[i]/math.log2(i+1) + idcg[i-1])
+
+            self.__dcg = dcg
+            self.__idcg = idcg
+
+        return self.__dcg, self.__idcg
