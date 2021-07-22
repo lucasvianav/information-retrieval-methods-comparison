0b2932c1021097ddbf85473487c7dcf4503c12a3
Lucas Viana <oviana@outlook.com>
2021-07-21 21:24:12 -0300

    refactors main.py and creates getMetrics()

--- source-code/main.py
+++ source-code/main.py
@@ -2,119 +2,127 @@
 from evaluation import Evaluation
 from index_class import Index
 from methods import probabilisticModel, vectorialModel
+from query_expansion import implicit_feedback
 from util import *
 
-FILTER_STOPWORDS = True
-STEM_WORDS = True
-
 database_contents = get_db_content()
-index = Index(database_contents, FILTER_STOPWORDS, STEM_WORDS)
-
 queries = get_queries()
 truth_sets = get_queries_truth_set()
 
-# interpol: first list is precision and second is recall
-# dcg: first item is DCG and second is IDCG
-metrics = {
-    'probab': {
-        'precision': 0.,
-        'recall': 0.,
-        'map': 0.,
-        'interpol': ([], []),
-        'dcg': ([], [])
-    },
+def getMetrics(filter_stopwords: bool, stem_words: bool, expand_queries: bool):
+    index = Index(database_contents, filter_stopwords, stem_words)
 
-    'vectorial': {
-        'precision': 0.,
-        'recall': 0.,
-        'map': 0.,
-        'interpol': ([], []),
-        'dcg': ([], [])
+    # interpol: first list is precision and second is recall
+    # dcg: first item is DCG and second is IDCG
+    metrics = {
+        'probab': {
+            'precision': 0.,
+            'recall': 0.,
+            'map': 0.,
+            'interpol': ([], []),
+            'dcg': ([], [])
+        },
+
+        'vectorial': {
+            'precision': 0.,
+            'recall': 0.,
+            'map': 0.,
+            'interpol': ([], []),
+            'dcg': ([], [])
+        }
     }
-}
 
-intersections = []
+    # loops through all queries
+    for query in queries:
+        truth_set = truth_sets[query['id']]
+        query_vector = parse_text(query['query'], filter_stopwords, stem_words)
 
-# loops through all queries
-for query in queries:
-    # this query's truth set
-    truth_set = truth_sets[query['id']]
+        # the returned rankings
+        probabilistic = probabilisticModel(query_vector, index)
+        vectorial = vectorialModel(query_vector, index)
 
-    query_vector = parse_text(query['query'], FILTER_STOPWORDS, STEM_WORDS)
+        # exands queries if necessary
+        if expand_queries:
+            new_query = implicit_feedback(index, query_vector, probabilistic, 2)
+            probabilistic = probabilisticModel(new_query, index)
 
-    # the returned rankings
-    probabilistic = probabilisticModel(query_vector, index)
-    vectorial = vectorialModel(query_vector, index)
+            new_query = implicit_feedback(index, query_vector, vectorial, 2)
+            vectorial = vectorialModel(new_query, index)
 
-    evalProb = Evaluation(probabilistic, truth_set)
-    evalVect = Evaluation(vectorial, truth_set)
+        evalProb = Evaluation(probabilistic, truth_set)
+        evalVect = Evaluation(vectorial, truth_set)
 
-    intersections.append(evalProb.__intersection)
+        # saves this query's probabilistic evaluation metrics
+        metrics['probab']['precision'] += evalProb.getPrecision()
+        metrics['probab']['recall']    += evalProb.getRecall()
+        metrics['probab']['map']       += evalProb.getMAP()
 
-    # saves this query's probabilistic evaluation metrics
-    metrics['probab']['precision'] += evalProb.getPrecision()
-    metrics['probab']['recall']    += evalProb.getRecall()
-    metrics['probab']['map']       += evalProb.getMAP()
+        dcg = metrics['probab']['dcg'][0]
+        interpol = metrics['probab']['interpol'][0]
 
-    dcg = metrics['probab']['dcg'][0]
-    interpol = metrics['probab']['interpol'][0]
-
-    metrics['probab']['dcg'][0]      = get_sum(dcg, evalProb.getDCG()[0])
-    metrics['probab']['dcg'][1]      = get_sum(dcg, evalProb.getDCG()[1])
-    metrics['probab']['interpol'][0] = get_sum(interpol,
-                                               evalProb.getInterpol()[0])
-    metrics['probab']['interpol'][1] = evalProb.getInterpol()[1]
-
-
-    # saves this query's probabilistic vectorial metrics
-    metrics['vectorial']['precision'] += evalVect.getPrecision()
-    metrics['vectorial']['recall']    += evalVect.getRecall()
-    metrics['vectorial']['map']       += evalVect.getMAP()
-
-    dcg = metrics['vectorial']['dcg'][0]
-    interpol = metrics['vectorial']['interpol'][0]
-
-    metrics['vectorial']['dcg'][0]      = get_sum(dcg, evalVect.getDCG()[0])
-    metrics['vectorial']['dcg'][1]      = get_sum(dcg, evalVect.getDCG()[1])
-    metrics['vectorial']['interpol'][0] = get_sum(interpol,
-                                               evalVect.getInterpol()[0])
-    metrics['vectorial']['interpol'][1] = evalVect.getInterpol()[1]
+        metrics['probab']['dcg'][0]      = get_sum(dcg, evalProb.getDCG()[0])
+        metrics['probab']['dcg'][1]      = get_sum(dcg, evalProb.getDCG()[1])
+        metrics['probab']['interpol'][0] = get_sum(interpol,
+                                                   evalProb.getInterpol()[0])
+        metrics['probab']['interpol'][1] = evalProb.getInterpol()[1]
 
 
-no_queries = len(queries)
+        # saves this query's probabilistic vectorial metrics
+        metrics['vectorial']['precision'] += evalVect.getPrecision()
+        metrics['vectorial']['recall']    += evalVect.getRecall()
+        metrics['vectorial']['map']       += evalVect.getMAP()
 
-# calculates the precisions' mean values
-probabilistic_precisions = get_division(metrics['probab']['interpol'],
+        dcg = metrics['vectorial']['dcg'][0]
+        interpol = metrics['vectorial']['interpol'][0]
+
+        metrics['vectorial']['dcg'][0]      = get_sum(dcg, evalVect.getDCG()[0])
+        metrics['vectorial']['dcg'][1]      = get_sum(dcg, evalVect.getDCG()[1])
+        metrics['vectorial']['interpol'][0] = get_sum(interpol,
+                                                   evalVect.getInterpol()[0])
+        metrics['vectorial']['interpol'][1] = evalVect.getInterpol()[1]
+
+
+    no_queries = len(queries)
+
+    # calculates the precisions' mean values
+    probabilistic_precisions = get_division(metrics['probab']['interpol'],
+                                            number=no_queries)
+    vectorial_precisions = get_division(metrics['vectorial']['interpol'],
                                         number=no_queries)
-vectorial_precisions = get_division(metrics['vectorial']['interpol'],
-                                    number=no_queries)
 
-# calculates the probabilistic model's DCG and IDCG
-probabilistic_dcg = get_division(metrics['probab']['dcg'][0],
-                                 number=no_queries)
-probabilistic_idcg = get_division(metrics['probab']['dcg'][1],
-                                  number=no_queries)
+    # calculates the probabilistic model's DCG and IDCG
+    probabilistic_dcg = get_division(metrics['probab']['dcg'][0],
+                                     number=no_queries)
+    probabilistic_idcg = get_division(metrics['probab']['dcg'][1],
+                                      number=no_queries)
 
-# calculates the vectorial model's DCG and IDCG
-vectorial_dcg = get_division(metrics['vectorial']['dcg'][0],
-                                 number=no_queries)
-vectorial_idcg = get_division(metrics['vectorial']['dcg'][1],
-                                  number=no_queries)
+    # calculates the vectorial model's DCG and IDCG
+    vectorial_dcg = get_division(metrics['vectorial']['dcg'][0],
+                                     number=no_queries)
+    vectorial_idcg = get_division(metrics['vectorial']['dcg'][1],
+                                      number=no_queries)
 
-metrics = {
-    'probab': {
-        'precision': metrics['probab']['precision']/no_queries,
-        'recall': metrics['probab']['recall']/no_queries,
-        'map': metrics['probab']['map']/no_queries,
-        'interpol': (probabilistic_precisions, metrics['probab']),
-        'ndcg': get_division(probabilistic_dcg, list2=probabilistic_idcg)
-    },
+    return {
+        'probab': {
+            'precision': metrics['probab']['precision']/no_queries,
+            'recall': metrics['probab']['recall']/no_queries,
+            'map': metrics['probab']['map']/no_queries,
+            'interpol': {
+                'precision': probabilistic_precisions,
+                'recall': metrics['probab']['interpol'][1]
+            },
+            'ndcg': get_division(probabilistic_dcg, list2=probabilistic_idcg)
+        },
 
-    'vectorial': {
-        'precision': 0.,
-        'recall': 0.,
-        'map': 0.,
-        'interpol': ([], []),
-        'ndcg': get_division(vectorial_dcg, list2=vectorial_idcg)
+        'vectorial': {
+            'precision': metrics['vectorial']['precision']/no_queries,
+            'recall': metrics['vectorial']['recall']/no_queries,
+            'map': metrics['vectorial']['map']/no_queries,
+            'interpol': {
+                'precision': vectorial_precisions,
+                'recall': metrics['probab']['interpol'][1]
+            },
+            'ndcg': get_division(vectorial_dcg, list2=vectorial_idcg)
+        }
     }
-}
+
